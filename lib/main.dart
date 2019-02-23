85a013732ee97f8444fa8b6468b7bb7a363bd4e3
ankitbhadu <2018csb1073@iitrpr.ac.in>
2019-02-23 22:03:20 +0530

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
-void main() => runApp(new MyApp());
+import 'HomePageFarmer.dart';
+void main()=> runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "talki",
-      theme: new ThemeData(primarySwatch: Colors.red),
-      home: new HomePage(),
-      color: Colors.yellow,
+      title: "Agrizone",
+      theme: new ThemeData(
+        primaryColor: Colors.green,
+        accentColor: Colors.greenAccent
+      ),
+      home: new HomePageFarmersbuy(),
     );
-
   }
 }
+
